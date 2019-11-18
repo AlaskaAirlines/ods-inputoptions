91d08b556769f549373303534fb5617948ddcc1e
Cameron Dashti <cameron.dashti@alaskaair.com>
2019-11-18 14:11:00 -0800

    refactor: split input options and group into separate components

---
+++ src/ods-inputoption-group.js
@@ -0,0 +1,76 @@
+// Copyright (c) Alaska Air. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+// ---------------------------------------------------------------------
+import { LitElement, html } from "lit-element";
+import { classMap } from 'lit-html/directives/class-map';
+
+// Import touch detection lib
+import 'focus-visible/dist/focus-visible.min.js';
+
+// Import the processed CSS file into the scope of the component
+import componentProperties from './tokens/componentShapeProperties-css.js';
+import styleCss from "./style-css.js";
+
+class OdsInputoptionGroup extends LitElement {
+  constructor() {
+    super();
+
+    this._selectable = 'ods-inputoption'
+  }
+
+  static get properties() {
+    return {
+      horizontal: { type: Boolean },
+      error:      { type: String },
+      for:        { type: String },
+      label:      { type: String }
+    };
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._items = this.getElementsByTagName(this._selectable) || [];
+  }
+
+  _handleInput({ target }) {
+    Array.from(this._items).forEach(el => {
+      el === target ?
+        el.setAttribute('checked', '') :
+        el.removeAttribute('checked');
+    });
+  }
+
+  _errorChange() {
+    Array.from(this._items).forEach(el => el.error = !!this.error);
+  }
+
+  render() {
+    let groupClasses = {
+      'displayFlex': (this.horizontal && this._items.length <= 3)
+    }
+
+    return html`
+      ${componentProperties}
+      ${styleCss}
+
+      ${this._errorChange()}
+
+      ${this.label ?
+        html`<label for="${this.for}" class="ods-label">${this.label}</label>` :
+        html``}
+
+      <div
+        @input="${this._handleInput}"
+        class="${classMap(groupClasses)}">
+        <slot></slot>
+      </div>
+
+      ${this.error ?
+        html`<p class="errorText">${this.error}</p>` :
+        html``}
+    `;
+  }
+}
+
+// define the name of the custom component
+customElements.define("ods-inputoption-group", OdsInputoptionGroup);
