da442d191648612ee7ae2dc2e62b94b6c23e92d1
Cameron Dashti <cameron.dashti@alaskaair.com>
2019-11-18 14:11:00 -0800

    refactor: split input options and group into separate components

---
+++ src/ods-inputoption.js
@@ -0,0 +1,71 @@
+// Copyright (c) Alaska Air. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+// ---------------------------------------------------------------------
+import { LitElement, html } from "lit-element";
+import { classMap } from 'lit-html/directives/class-map';
+
+// Import touch detection lib
+import 'focus-visible/dist/focus-visible.min.js';
+
+// Import the processed CSS file into the scope of the component
+import componentProperties from './tokens/componentShapeProperties-css.js';
+import styleCss from "./style-css.js";
+
+export default class OdsInputoption extends LitElement {
+  static get properties() {
+    return {
+      checked:  { type: Boolean, reflect: true },
+      disabled: { type: Boolean },
+      required: { type: Boolean },
+      error:    { type: String },
+      for:      { type: String },
+      id:       { type: String },
+      label:    { type: String },
+      name:     { type: String },
+      type:     { type: String },
+      value:    { type: String },
+    };
+  }
+
+  handleChange({target}) {
+    this.checked = target.checked
+  }
+
+  render() {
+    let labelClasses = {
+      'ods-inputLabel': true,
+      'ods-inputLabel--checkbox': this.type === 'checkbox',
+      'ods-inputLabel--radio': this.type === 'radio',
+      'errorBorder': !!this.error
+    }
+
+    return html`
+      ${componentProperties}
+      ${styleCss}
+
+      <div class="ods-inputGroup" @change="${this.handleChange}">
+        <input
+          class="util_displayHiddenVisually ods-inputOption"
+          @change="${this.handleChange}"
+          ?disabled="${this.disabled}"
+          ?required="${this.required}"
+          .checked="${this.checked}"
+          id="${this.id}-id"
+          name="${this.name}"
+          type="${this.type}"
+          value="${this.value}"
+        />
+
+        <label
+          for="${this.id}-id"
+          class="${classMap(labelClasses)}"
+        >
+          ${this.label}
+        </label>
+      </div>
+    `;
+  }
+}
+
+// define the name of the custom component
+customElements.define("ods-inputoption", OdsInputoption);
